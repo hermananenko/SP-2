3749189c427e2c76243a412d7c9fbe63da7e99fa
hermananenko <hermananenko@gmail.com>
2020-11-11 17:12:31 +0200

    Lab_6

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         try {
-            Lexer lexer = new Lexer(FileWorker.read("/5-2-Java-IO-83-Ananenko.py"));
+            Lexer lexer = new Lexer(FileWorker.read("/6-2-Java-IO-83-Ananenko.py"));
             List<Token> tokenList = lexer.tokenize();
 
             for (Token tk : tokenList) {
@@ -19,7 +19,7 @@ public class Main {
             Parser parser = new Parser(tokenList);
             List<Statement> stmtList = parser.parse();
             String code = new CodeGenerator(stmtList, parser.orIsExist, parser.ltIsExist, parser.gtIsExist, parser.eqIsExist).getCode();
-            FileWorker.write("5-2-Java-IO-83-Ananenko.asm", code);
+            FileWorker.write("6-2-Java-IO-83-Ananenko.asm", code);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
